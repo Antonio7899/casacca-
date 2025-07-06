851dabde9163208b4f60cac9fb98c7f360544902
anto82 <antonioraso85@gmail.com>
2025-07-06 16:53:38 +0200

    Pulizia progetto: eliminati file inutili

--- app_simple.py
+++
@@ -1,3 +0,0 @@
-print("Server di test - Funziona!")
-print("Apri il browser su: http://127.0.0.1:5000")
-input("Premi Invio per uscire...") 
--- start_server.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo === Avvio Server Flask ===
-cd /d "C:\Users\anton\Desktop\cursor"
-call .venv\Scripts\activate
-echo Server Flask avviato su http://localhost:5000
-echo Premi CTRL+C per fermare il server
-python app.py
-pause 
